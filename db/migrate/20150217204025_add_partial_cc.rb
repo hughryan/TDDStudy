8189eccbf26c71aca69d0f2553d7a3f77af4caa7
Michael Hilton <michael.hilton@gmail.com>
2015-02-17 13:42:55 -0800

    added migration

--- db/migrate/20150217204025_add_partial_cc.rb
+++ db/migrate/20150217204025_add_partial_cc.rb
@@ -1,6 +1,6 @@
 class AddPartialCc < ActiveRecord::Migration
   def change
-    add_column :sessions, :test_cycomatic_complexity, :float
-    add_column :sessions, :production_cycomatic_complexity, :float
+    add_column :sessions, :test_cyclomatic_complexity, :float
+    add_column :sessions, :production_cyclomatic_complexity, :float
   end
 end
